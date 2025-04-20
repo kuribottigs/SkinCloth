2b6bc72b6388012b20f9c2cc4085941c092c15b4
Kuri <kozuma0721@gmail.com>
2025-04-20 23:17:52 +0900

    some fixes and add fictions all on off

--- src/client/java/com/samesama/skincloth/client/SkinClothClient.java
+++ src/client/java/com/samesama/skincloth/client/SkinClothClient.java
@@ -4,13 +4,15 @@ import com.samesama.skincloth.SkinCloth;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
 public class SkinClothClient implements ClientModInitializer {
     // --- キーバインドの定義 ---
-    private static KeyBinding[] keys = new KeyBinding[KeyCode.values().length];
+    private static final KeyBinding[] keys = new KeyBinding[KeyCode.values().length];
     private static KeyBinding all_on;
+    private static KeyBinding all_off;
     private static final String KEY_CATEGORY = "key.categories." + SkinCloth.MOD_ID;
 
     @Override
@@ -26,6 +28,18 @@ public class SkinClothClient implements ClientModInitializer {
                     KEY_CATEGORY                  // キーバインドのカテゴリ
             ));
         }
+        all_on = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key." + SkinCloth.MOD_ID + ".allon", // 翻訳キー (langファイルで定義)
+                InputUtil.Type.KEYSYM,        // 入力タイプ (キーボード)
+                InputUtil.UNKNOWN_KEY.getCode(),              // デフォルトのキー (Gキー)
+                KEY_CATEGORY                  // キーバインドのカテゴリ
+        ));
+        all_off = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key." + SkinCloth.MOD_ID + ".alloff", // 翻訳キー (langファイルで定義)
+                InputUtil.Type.KEYSYM,        // 入力タイプ (キーボード)
+                InputUtil.UNKNOWN_KEY.getCode(),              // デフォルトのキー (Gキー)
+                KEY_CATEGORY                  // キーバインドのカテゴリ
+        ));
 
         // --- キー入力の処理 ---
         // クライアントのティック（ゲームの更新タイミング）ごとにキーが押されたかチェック
@@ -37,16 +51,35 @@ public class SkinClothClient implements ClientModInitializer {
                     if (client.player != null) {
                         //client.player.sendMessage(Text.literal("First Key Pressed!"), false);
                         // ここにキーが押されたときのアクションを記述
-                        if(client.options.isPlayerModelPartEnabled(keyCode.getPart())){
-                            client.options.togglePlayerModelPart(keyCode.getPart(),false);
-                        }else{
-                            client.options.togglePlayerModelPart(keyCode.getPart(),true);
-                        }
+                        client.options.togglePlayerModelPart(keyCode.getPart(), !client.options.isPlayerModelPartEnabled(keyCode.getPart()));
                         client.options.write();
                     }
                 }
             });
         }
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            // firstKeyBindingが押された瞬間を検出
+            while (all_on.wasPressed() || all_off.wasPressed()) {
+                // プレイヤーが存在する場合のみ実行 (ワールドに入っていない場合などはnull)
+                if (all_on.wasPressed()) {
+                    AllOnOff(true,client);
+                }
+                if(all_off.wasPressed()){
+                    AllOnOff(false,client);
+                }
+            }
+        });
 
+    }
+
+    public void AllOnOff(boolean b, MinecraftClient client){
+        if (client.player != null) {
+            //client.player.sendMessage(Text.literal("First Key Pressed!"), false);
+            // ここにキーが押されたときのアクションを記述
+            for(KeyCode keyCode : KeyCode.values()) {
+                client.options.togglePlayerModelPart(keyCode.getPart(), b);
+                client.options.write();
+            }
+        }
     }
 }
--- src/client/java/com/samesama/skincloth/client/datagen/SkinClothEnglishLangProvider.java
+++ src/client/java/com/samesama/skincloth/client/datagen/SkinClothEnglishLangProvider.java
@@ -1,5 +1,6 @@
 package com.samesama.skincloth.client.datagen;
 
+import com.samesama.skincloth.SkinCloth;
 import com.samesama.skincloth.client.KeyCode;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
@@ -15,6 +16,8 @@ public class SkinClothEnglishLangProvider extends FabricLanguageProvider {
     @Override
     public void generateTranslations(TranslationBuilder translationBuilder) {
         translationBuilder.add("key.categories.skincloth", "Skin Cloth Key");
+        translationBuilder.add("key." + SkinCloth.MOD_ID + ".allon","All On");
+        translationBuilder.add("key." + SkinCloth.MOD_ID + ".alloff","All Off");
         for(KeyCode keyCode : KeyCode.values()){
                     //"key.skincloth." + keyCode.getName(),
             translationBuilder.add(keyCode.getLangName(), keyCode.getUsLang());
