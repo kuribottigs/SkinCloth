4a37352dcb724e98f7bb6ca83558de6cfc91750b
Kuri <kozuma0721@gmail.com>
2025-04-21 23:26:35 +0900

    1.1.0.B

--- src/client/java/com/samesama/skincloth/client/KeyCode.java
+++ src/client/java/com/samesama/skincloth/client/KeyCode.java
@@ -3,6 +3,8 @@ package com.samesama.skincloth.client;
 import com.samesama.skincloth.SkinCloth;
 import net.minecraft.client.render.entity.PlayerModelPart;
 
+import static com.samesama.skincloth.client.SkinClothClient.CONFIG;
+
 public enum KeyCode {
     CAPE(0, "cape",PlayerModelPart.CAPE,"Cape"),
     JACKET(1, "jacket",PlayerModelPart.JACKET,"Jacket"),
@@ -18,6 +20,7 @@ public enum KeyCode {
     private final String usLang;
     private final PlayerModelPart part;
     private final String langName;
+    //private final boolean takeon;
 
     private KeyCode(int id, String name, PlayerModelPart part, String us_lang ) {
         this.id = id;
@@ -25,7 +28,7 @@ public enum KeyCode {
         this.part = part;
         this.usLang = us_lang;
         this.langName = "key." + SkinCloth.MOD_ID + name;
-
+        //this.takeon = b;
     }
 
     public int getId() { return id; }
@@ -39,4 +42,5 @@ public enum KeyCode {
     public String getUsLang(){ return this.usLang; }
 
     public String getLangName(){ return this.langName; }
+    public boolean isTakeon(){ return this.takeon;}
 }
--- src/client/java/com/samesama/skincloth/client/config/SkinClothConfig.java
+++ src/client/java/com/samesama/skincloth/client/config/SkinClothConfig.java
@@ -8,8 +8,21 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = SkinCloth.MOD_ID)
 public class SkinClothConfig implements ConfigData {
-    @Comment("This is whether to turn on hat when AllOnOff is selected")
+    @Comment("This is whether to turn on cape when AllOnOff is selected")
     public boolean enableAllCape = true;
+    @Comment("This is whether to turn on jacket when AllOnOff is selected")
+    public boolean enableAllJacket = true;
+    @Comment("This is whether to turn on left sleeve when AllOnOff is selected")
+    public boolean enableAllLeftSleeve = true;
+    @Comment("This is whether to turn on right sleeve when AllOnOff is selected")
+    public boolean enableAllRightSleeve = true;
+    @Comment("This is whether to turn on left pants legs when AllOnOff is selected")
+    public boolean enableAllLeftPantsLegs = true;
+    @Comment("This is whether to turn on right pants legs when AllOnOff is selected")
+    public boolean enableAllRightPantsLegs = true;
+    @Comment("This is whether to turn on hat when AllOnOff is selected")
+    public boolean enableAllHat = true;
+
 
     @ConfigEntry.Gui.Excluded
     public boolean enableALL = true;
