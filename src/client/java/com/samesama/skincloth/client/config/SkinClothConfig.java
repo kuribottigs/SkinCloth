fe36b1a50c4be2152858121a43f4b8eca03658cd
Kuri <kozuma0721@gmail.com>
2025-04-21 23:30:15 +0900

    yotei

--- src/client/java/com/samesama/skincloth/client/config/SkinClothConfig.java
+++ src/client/java/com/samesama/skincloth/client/config/SkinClothConfig.java
@@ -10,18 +10,20 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 public class SkinClothConfig implements ConfigData {
     @Comment("This is whether to turn on cape when AllOnOff is selected")
     public boolean enableAllCape = true;
-    @Comment("This is whether to turn on jacket when AllOnOff is selected")
+    @Comment("Not implemented : This is whether to turn on jacket when AllOnOff is selected")
     public boolean enableAllJacket = true;
-    @Comment("This is whether to turn on left sleeve when AllOnOff is selected")
+    @Comment("Not implemented : This is whether to turn on left sleeve when AllOnOff is selected")
     public boolean enableAllLeftSleeve = true;
-    @Comment("This is whether to turn on right sleeve when AllOnOff is selected")
+    @Comment("Not implemented : This is whether to turn on right sleeve when AllOnOff is selected")
     public boolean enableAllRightSleeve = true;
-    @Comment("This is whether to turn on left pants legs when AllOnOff is selected")
+    @Comment("Not implemented : This is whether to turn on left pants legs when AllOnOff is selected")
     public boolean enableAllLeftPantsLegs = true;
-    @Comment("This is whether to turn on right pants legs when AllOnOff is selected")
+    @Comment("Not implemented :  is whether to turn on right pants legs when AllOnOff is selected")
     public boolean enableAllRightPantsLegs = true;
-    @Comment("This is whether to turn on hat when AllOnOff is selected")
+    @Comment("Not implemented : This is whether to turn on hat when AllOnOff is selected")
     public boolean enableAllHat = true;
+    @Comment("Not implemented : This is whether to turn on hat when AllOnOff is selected")
+    public boolean enableOverlay = false;
 
 
     @ConfigEntry.Gui.Excluded
